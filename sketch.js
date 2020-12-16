56ba5d252d87bd21733c2276c1f49e1c51f5c7c1
AlphaSIxer <73114468+AlphaSIxer@users.noreply.github.com>
2020-12-16 17:20:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -19,10 +18,10 @@ function setup() {
 	roof1= new Roof(150,20)
 	b1= new Ball(50,250)
 	b2= new Ball(100,250)
-	b3= new Ball(50,250)
-	rope1= new Rope(b1.body,roof1.body,-100)
-	rope2= new Rope(b2.body,oof1.body,-50)
-	rope3= new Rope(b3.body,roof1.body,0)
+	b3= new Ball(150,250)
+	rope1= new Rope(b1.body,roof1.roof,-100)
+	rope2= new Rope(b2.body,roof1.roof,-50)
+	rope3= new Rope(b3.body,roof1.roof,0)
 	
 	Engine.run(engine);
   
@@ -34,7 +33,7 @@ function setup() {
   	Engine.update(engine);
   
 	roof1.display();
-	b1.display
+	b1.display();
 	rope1.display();
 	b2.display();
 	rope2.display();
@@ -44,6 +43,9 @@ function setup() {
   	drawSprites();
  
 	}	
+
+
+
 
 
 
